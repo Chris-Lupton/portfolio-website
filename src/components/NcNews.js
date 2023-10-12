156c94c34a646ab6fa7bc0704a2c730022598000
Chris-Lupton <kitlupton@hotmail.com>
2023-10-12 16:54:14 +0100

    adds nc news project

---
+++ src/components/NcNews.js
@@ -0,0 +1,15 @@
+
+export function NcNews() {
+    
+    return (
+        <section className='project'>
+            <h1>Nc News</h1>
+            <p>NC News is a social media style website that displays articles and comments, developed during my time on the Northcoders bootcamp. Built using PSQL Express.js React.js and Node.</p>
+            <div className="button-box">
+                <a className='project-button' target="_blank" href='https://github.com/Chris-Lupton/nc-news-project-front-end'><b>GitHub</b> <small>(Front-end)</small></a>
+                <a className='project-button' target="_blank" href='https://github.com/Chris-Lupton/be-nc-news-project'><p><b>GitHub</b> <small>(Back-end)</small></p></a>
+                <a className='project-button' target="_blank" href='https://chrisl-nc-news.netlify.app/'><b>View Site</b></a>
+            </div>
+        </section>
+    )
+}
