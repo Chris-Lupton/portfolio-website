33c5ba8334d6903aefe4b2e72651baaf1d76ffb5
Chris-Lupton <kitlupton@hotmail.com>
2023-10-12 16:54:55 +0100

    adds roman calculator project

---
+++ src/components/RomanCalculator.js
@@ -0,0 +1,13 @@
+export function RomanCalculator() {
+
+    return (
+        <section className='project'>
+            <h1>Roman Calculator</h1>
+            <p>A calculator mobile app that uses roman numerals. Built using React Native and Javascript as a short fun project. It can be used to make simple calculations, while also handling fractions, and it will convert any numeral to its decimal value.</p>
+            <div className="button-box">
+                <a className='project-button' target="_blank" href='https://github.com/Chris-Lupton/roman-calculator'><b>GitHub</b></a>
+                <a className='project-button' target="_blank" href='https://roman-numerals-mobile-calculator.netlify.app/'><b>View Site</b></a>
+            </div>
+        </section>
+    )
+}
