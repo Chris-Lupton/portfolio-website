5004d5d5005e1fa3b374cdffc3caed618303d201
Chris-Lupton <kitlupton@hotmail.com>
2023-10-14 15:43:18 +0100

    updates project description

--- src/components/NcNews.js
+++ src/components/NcNews.js
@@ -1,10 +1,14 @@
 
-export function NcNews({ blur }) {
+export function NcNews({ blur, colour }) {
     
     return (
         <section className='project' id={blur}>
-            <h1>Nc News</h1>
-            <p>NC News is a social media style website that displays articles and comments, developed during my time on the Northcoders bootcamp. Built using PSQL Express.js React.js and Node.</p>
+            <div className="left-bar" id={colour}></div>
+            <h1>NC News</h1>
+            <p className="text">NC News is a social media style website that displays articles, and comments for different users, developed during my time on the Northcoders bootcamp. You can filter articles by topic, date or popularity. It was designed with a mobile first approach and styled with CSS to make it fully responsive for all screens. Special care was taken to make it as accessable as possible.
+            <br/>
+            <br/>
+            PSQL | Express | React | Node</p>
             <div className="button-box">
                 <a className='project-button' target="_blank" href='https://github.com/Chris-Lupton/nc-news-project-front-end'><b>GitHub</b> <small>(Front-end)</small></a>
                 <a className='project-button' target="_blank" href='https://github.com/Chris-Lupton/be-nc-news-project'><p><b>GitHub</b> <small>(Back-end)</small></p></a>
